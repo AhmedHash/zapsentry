7ffc6d4d8b10686729a41c73045b9d8810f026b9
Ahmed Hashem <ahashem302@gmail.com>
2020-04-19 23:40:55 +0200

    using zap SugaredLogger

--- logger.go
+++ logger.go
@@ -5,7 +5,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func AttachCoreToLogger(sentryCore zapcore.Core, l *zap.Logger) *zap.Logger {
+func AttachCoreToLogger(sentryCore zapcore.Core, l *zap.SugaredLogger) *zap.SugaredLogger {
 	return l.WithOptions(zap.WrapCore(func(core zapcore.Core) zapcore.Core {
 		return zapcore.NewTee(core, sentryCore)
 	}))
